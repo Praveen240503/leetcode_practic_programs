0c14ef6852d42affae440159081d2ef32cd9640d
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-02-15 21:02:07 +0530

    Time: 74 ms (99.99%) | Memory: 82.9 MB (36.53%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.cpp
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     long long largestPerimeter(vector<int>& nums) {
+        ios_base::sync_with_stdio(false); 
+        cin.tie(NULL);
+        cout.tie(NULL);
         sort(nums.begin(),nums.end());
         long long ans=INT_MIN,n=nums[0];
         int f=0;
