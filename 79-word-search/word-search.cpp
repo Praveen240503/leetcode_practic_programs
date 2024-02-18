45ec957c0f4f85c5ee05163274e36fc0fd417c42
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-02-18 20:56:35 +0530

    Time: 237 ms (87.97%) | Memory: 9.9 MB (39.37%) - LeetSync

---
+++ 79-word-search/word-search.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool dfs(int i, int j, int n, int m, vector<vector<char>>& board, 
+            vector<vector<int>>&vis, int k, string &word) {
+        if (k == word.size()) return true;
+        if (i == n || i < 0 || j < 0 || j == m || vis[i][j]==1 || board[i][j] != word[k]) {
+            return false; 
+        }
+        vis[i][j]=1;
+        bool call1 = dfs(i + 1, j, n, m, board, vis, k + 1, word);
+        bool call2 = dfs(i - 1, j, n, m, board, vis, k + 1, word);
+        bool call3 = dfs(i, j + 1, n, m, board, vis, k + 1, word);
+        bool call4 = dfs(i, j - 1, n, m, board, vis, k + 1, word);
+        vis[i][j]=0;
+        return call1 || call2 || call3 || call4;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+        int k = word.size(); 
+       // bool c=false; 
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                vector<vector<int>>vis(n,vector<int>(m,0));
+                if (board[i][j]==word[0]) {
+                    if(dfs(i,j,n,m,board,vis,0,word)) return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
