248dd4a1468132986c188133ea65540e5cfbfa8a
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-02-20 22:56:58 +0530

    Time: 2 ms (89.71%) | Memory: 11.2 MB (62.94%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    int dp[1001];
-    bool bunch(int i, string &s , set<string> & st){
+    int dp[301];
+    bool bunch(int i, string &s , unordered_set<string> & st){
         if(i==s.size())return dp[i]=1;
         if(dp[i] != -1) return dp[i];
         string temp;
@@ -17,7 +17,7 @@ public:
         cin.tie(NULL);
         cout.tie(NULL);
         memset(dp,-1,sizeof(dp));
-        set<string>st;
+        unordered_set<string>st;
         for(auto it : wordDict){
             st.insert(it);
         }
