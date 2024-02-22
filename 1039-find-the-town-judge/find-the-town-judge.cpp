a8dbc6565bf918e9a7d40d7b3f9550978a015113
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-02-22 17:03:05 +0530

    Time: 134 ms (67.51%) | Memory: 74.1 MB (12.54%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         if(n==1)return 1;
-        map<int,int>mp1;
-        map<int,int>mp2;
+        unordered_map<int,int>mp1;
+        unordered_map<int,int>mp2;
         for(auto it:trust){
             mp1[it[0]]++;
             mp2[it[1]]++;
