9b036ee47735eb8ff11d488e20cf55b85811dde5
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-03-10 23:15:18 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (26.85%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(nullptr);
-        cout.tie(nullptr);
-        vector<int> ans;
-        unordered_set<int>one, two;
-        for(int i = 0; i < nums1.size(); i++) {
-            one.insert(nums1[i]);
+        unordered_map<int,int> mp;
+        vector<int> tr;
+        for(int i = 0; i < nums1.size(); i++)
+        {
+            mp[nums1[i]]++;
         }
-        for(int i = 0; i < nums2.size(); i++) {
-            if(one.find(nums2[i]) != one.end()) {
-                two.insert(nums2[i]);
+        for(int i = 0;i < nums2.size(); i++)
+        {
+            if(mp.find(nums2[i]) != mp.end())
+            {
+                tr.push_back(nums2[i]);
+                mp.erase(nums2[i]);
             }
         }
-        for(auto it:two){
-            ans.push_back(it);
-
-        }
-        return ans;
+        return tr;
     }
 };
