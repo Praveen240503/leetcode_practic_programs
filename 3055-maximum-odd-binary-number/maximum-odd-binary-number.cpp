ad2da8e550b015cf2289e0b5bda5fa35795e68d3
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-03-11 02:18:12 +0530

    Time: 9 ms (7.91%) | Memory: 10.7 MB (5.49%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(NULL);
+        cout.tie(NULL);
         sort(s.rbegin(), s.rend());
         // cout<<s;
         for(int i = s.size()-1; i >= 0; i--){
