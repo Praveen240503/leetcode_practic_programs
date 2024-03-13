b53f2966e9fc4ece8319e7ffdaa0bd8e277b8e2a
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-03-13 21:51:34 +0530

    Time: 48 ms (23.36%) | Memory: 32.5 MB (5.24%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int row = mat.size();
         int col = mat[0].size();
         vector <int> ans;
