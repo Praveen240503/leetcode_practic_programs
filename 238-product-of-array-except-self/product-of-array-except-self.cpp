36dba401d425185eac18e1d5d8bf41cea4cbc56a
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-03-15 18:45:44 +0530

    Time: 11 ms (90.04%) | Memory: 26.4 MB (72.09%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>tr(n,0);
+        vector<int>zero;
+        
+        for(int i = 1; i < n; i++) {
+            // temp *= nums[i];
+            if(nums[i] == 0)zero.push_back(i);
+        }
+        if(zero.size() == 0){
+            int temp = 1;
+            for(int i = 1; i < n; i++) {
+                temp *= nums[i];
+            }
+            // cout<<temp;
+            tr[0] = temp;
+            for(int i = 1; i < n; i++) {
+                int tem = (tr[i-1] * nums[i-1]) / nums[i];
+                tr[i] = tem;
+            }
+        }
+        if(zero.size() == 1){
+            int tem = 1;
+            for(int i = 0; i < n; i++ ) {
+                if(i != zero[0])tem *= nums[i];
+            }
+            tr[zero[0]] = tem;
+            tem = 1;
+        }
+        return tr;
+    }
+};
