8b2e9b27a7c43f5616742a40ee701ee74fde583f
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-06-10 09:05:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (10.73%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         vector<int>expected = heights;
         sort(expected.begin(), expected.end());
         int count = 0;
