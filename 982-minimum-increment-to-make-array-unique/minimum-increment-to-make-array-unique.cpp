290170a8f79bb614a07ffc23ac19ca6a77c29823
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-06-14 14:04:59 +0530

    Time: 82 ms (97.24%) | Memory: 68.9 MB (96.85%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.cpp
@@ -4,24 +4,26 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         cout.tie(NULL);
-        vector<int> dums = nums;
+        // vector<int> dums = nums;
         sort(nums.begin(), nums.end());
         // for(auto it : nums)cout<<it<<" ";
         // cout<<"\n";
         int c = 0;
         for(int i = 1; i < nums.size(); i++){
             if(nums[i-1] == nums[i]){
+                c+=1;
                 nums[i] += 1;
             }
             else if( nums[i-1] > nums[i]){
+                c += (nums[i-1] - nums[i] + 1);
                 nums[i] = nums[i] + (nums[i-1] - nums[i]) +1;
             }
         }
         
         // for(auto it : nums)cout<<it<<" ";
-        for(int i = 0; i < nums.size(); i++){
-            c+= nums[i] - dums[i];
-        }
+        // for(int i = 0; i < nums.size(); i++){
+        //     c+= nums[i] - dums[i];
+        // }
         return c;
     }
 };
