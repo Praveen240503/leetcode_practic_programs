8b8625929220fd7e90880a1f8fb2fcdf7313f46c
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-06-15 12:09:23 +0530

    Time: 569 ms (64.25%) | Memory: 331.8 MB (38.06%) - LeetSync

---
+++ 3442-maximum-total-reward-using-operations-i/maximum-total-reward-using-operations-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int func(int ind , int total, vector<int>&rewardValues, int n, vector<vector<int>>&dp){
+        if(ind >= n)return total;
+
+        if(dp[ind][total] != -1) return dp[ind][total];
+        // pick
+        int left = (total < rewardValues[ind]) ? func(ind +1, total + rewardValues[ind],rewardValues,n, dp):total;
+
+
+        // non pick
+        int right = func(ind+1, total, rewardValues , n,dp);
+
+        return dp[ind][total] = max(left, right);
+    }
+    int maxTotalReward(vector<int>& rewardValues) {
+        int n = rewardValues.size();
+        sort(rewardValues.begin(), rewardValues.end());
+        int maxi = rewardValues.back();
+        cout<< maxi;
+        vector<vector<int>>dp(n+1,vector<int>(4000+1,-1));
+        // cout<<maxi;
+        return func(0,0,rewardValues,n, dp);
+    }
+};
