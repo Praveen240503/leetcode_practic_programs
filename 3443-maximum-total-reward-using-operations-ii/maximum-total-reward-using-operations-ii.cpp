52436d27447f8a0e4af2513e207f26bf1a99a0c3
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2024-06-15 14:24:09 +0530

    Time: 2829 ms (5.01%) | Memory: 50.9 MB (59.21%) - LeetSync

---
+++ 3443-maximum-total-reward-using-operations-ii/maximum-total-reward-using-operations-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxTotalReward(vector<int>& rewardValues) {
+        bitset<500000> dp, mask;
+        int n = rewardValues.size();
+        sort(rewardValues.begin(),rewardValues.end());
+        dp[0] = 1;
+        int mask_id = 0;
+        int close = rewardValues.back()-1;
+        for(int i = 0 ; i + 1 < n ; ++i){
+            if(mask_id < rewardValues[i]){
+                while(mask_id < rewardValues[i]){
+                    mask[mask_id++]=1;
+                }
+
+                dp |=(dp & mask) << rewardValues[i];
+            }
+        }
+    
+        while(!dp[close]){
+            close--;
+        }
+        return close + rewardValues.back();
+        
+    }
+};
