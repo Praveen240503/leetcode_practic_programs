cb03ed11f1a92d5e0294fa569922097702bfe14e
praveen_0006 <96769580+Praveen240503@users.noreply.github.com>
2025-03-04 14:55:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (41.55%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        ios_base::sync_with_stdio;
-        cin.tie(NULL);
-        cout.tie(NULL);
-        int near_power = 1;
-        while(near_power <= n){
-            near_power*=3;
+        while(n){
+            if(n%3 == 2)return 0;
+            n/=3;
         }
-        // cout<<near_power;
-        while(n>0 and near_power>0){
-            if(near_power <= n){
-                n-=near_power;
-            }
-            near_power/=3;
-        }
-        return n==0;
+        return 1;
     }
 };
